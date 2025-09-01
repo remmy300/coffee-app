6324f81aa05a4dbb4e3a22be11addeb918612fa7
JENTAH REHEMA <jentahrehema7@gmail.com>
2025-09-01 21:32:51 +0300

    Adding .env file

--- frontend/src/utils/apiProducts.ts
+++ frontend/src/utils/apiProducts.ts
@@ -1,14 +1,22 @@
 import type { CoffeeProduct } from "@/types";
 
+const API_URL = import.meta.env.VITE_API_URL;
+
+type ApiResponse<T> = {
+  data: T;
+  message?: string;
+};
+
 export const fetchProducts = async (): Promise<CoffeeProduct[]> => {
-  const res = await fetch("http://localhost:4000/api/products");
-  const json = await res.json();
+  const res = await fetch(`${API_URL}/products`);
+  if (!res.ok) throw new Error("Failed to fetch products");
+  const json: ApiResponse<CoffeeProduct[]> = await res.json();
   return json.data;
 };
 
 export const fetchProductById = async (id: string): Promise<CoffeeProduct> => {
-  const res = await fetch(`http://localhost:4000/api/products/${id}`);
+  const res = await fetch(`${API_URL}/products/${id}`);
   if (!res.ok) throw new Error("Failed to fetch product");
-  const json = await res.json();
+  const json: ApiResponse<CoffeeProduct> = await res.json();
   return json.data;
 };
